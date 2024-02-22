024a1403e53d57a832be60e4663e6c63f2041dee
mgjr-73 <113564084+mgjr-73@users.noreply.github.com>
2024-02-21 22:38:15 -0500

    Update D001_band_name_gen.py
    Moved comments and adjusted line spaces for readability

--- D001_band_name_gen.py
+++ D001_band_name_gen.py
@@ -5,15 +5,14 @@
 #5. Make sure the input cursor shows on a new line:
 # Solution: https://replit.com/@appbrewery/band-name-generator-end
 
-#1
-print("Hello and welcome to the band name generator!")
-#2
-city = input("What's the name of the city you grew up in?\n> ")
-#3
-pet_name = input("What's a good name for a pet?\n> ")
-#4 Notice application of title() method
-print('Your band name is "' + city.title() + ' ' + pet_name + '"!')
+print("Hello and welcome to the band name generator!")  #1
+city = input("What's the name of the city you grew up in?\n> ")  #2
+pet_name = input("What's a good name for a pet?\n> ")  #3
+
+print('Your band name is "' + city.title() + ' ' + pet_name + '"!')  #4 Notice application of title() method
+
 # Another way to write #4 is to use .format, where positional arguments are passed into the curly braces.
 print('Your band name is "{} {}"!'.format(city, pet_name).title())
+
 # Yet another way to write it is to use f-string
 print(f'Your band name is "{city} {pet_name}"!')
