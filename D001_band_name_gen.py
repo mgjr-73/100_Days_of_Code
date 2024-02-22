ed9efff49bd2838b11cdef5ed766b7097c4af4c4
mgjr-73 <mgjr73@outlook.com>
2024-02-21 22:24:12 -0500

    init Day 1 project

---
+++ D001_band_name_gen.py
@@ -0,0 +1,19 @@
+#1. Create a greeting for your program.
+#2. Ask the user for the city that they grew up in.
+#3. Ask the user for the name of a pet.
+#4. Combine the name of their city and pet and show them their band name.
+#5. Make sure the input cursor shows on a new line:
+# Solution: https://replit.com/@appbrewery/band-name-generator-end
+
+#1
+print("Hello and welcome to the band name generator!")
+#2
+city = input("What's the name of the city you grew up in?\n> ")
+#3
+pet_name = input("What's a good name for a pet?\n> ")
+#4 Notice application of title() method
+print('Your band name is "' + city.title() + ' ' + pet_name + '"!')
+# Another way to write #4 is to use .format, where positional arguments are passed into the curly braces.
+print('Your band name is "{} {}"!'.format(city, pet_name).title())
+# Yet another way to write it is to use f-string
+print(f'Your band name is "{city} {pet_name}"!')
