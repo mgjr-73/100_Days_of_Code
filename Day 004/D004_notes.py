9332699d8a890a0560c698a8b5fd9e9d7b3ad08d
mgjr-73 <mgjr73@outlook.com>
2024-04-05 08:12:10 -0400

    Day 4 exercises.

---
+++ Day 004/D004_notes.py
@@ -0,0 +1,34 @@
+#==================
+# Randomization
+#==================
+'''
+For documentation on random module, go to askpython.com
+'''
+import random
+
+random_integer = random.randint(1, 10)
+print(random_integer)  # prints random number within range 1 - 10.
+
+random_float = random.random()
+print(random_float)  # By default, only range from 0.00000... to 0.99999...
+print(random_float * 5)  # Expands float to higher numbers, in this case from 0.00000... to 4.99999...
+
+#==================
+# Lists
+#==================
+'''
+See docs.python.org/3/tutorial/datastructures.html
+'''
+us_states = ["Maryland", "New York", "Delaware"]
+
+print(us_states[0])  # Accessing list by index number
+
+first,second,third = us_states  # This is unpacking the list and assigning items to positional variables
+print(second)
+
+print(us_states[2])
+us_states[2] = "Deelaware"  # Changes value at index number 2.
+print(us_states[2])
+
+us_states.extend(["California", "Texas", "Utah"])
+print(us_states)
--- README.md
+++ README.md
@@ -34,3 +34,12 @@
   - [ascii art](https://ascii.co.uk/art)
   - [Treasure Island Project](https://replit.com/@appbrewery/treasure-island-end)
   - [Treasure Island Flowchart](https://viewer.diagrams.net/index.html?highlight=0000ff&edit=_blank&layers=1&nav=1&title=Treasure%20Island%20Conditional.drawio#Uhttps%3A%2F%2Fdrive.google.com%2Fuc%3Fid%3D1oDe4ehjWZipYRsVfeAx2HyB7LCQ8_Fvi%26export%3Ddownload#%7B%22pageId%22%3A%22C5RBs43oDa-KdzZeNtuy%22%7D)
+
+## Day 3 lesson topics:
+- Randomization
+  - random module
+  - random.randint
+  - random.random
+- Offset and Appending Items on lists
+  - A list uses square brackets [item1, item2]
+  - Index order
