eebc9e27631ca73efe839afc8eefbcd9397d9b65
mgjr-73 <mgjr73@outlook.com>
2024-07-12 00:08:56 -0400

    gitadded hitting and final total functions

--- Day_011/blackjack.py
+++ Day_011/blackjack.py
@@ -8,62 +8,153 @@
 # Step 5. Ask to play again or exit the program.
 
 import random
+from os import system, name
+
 
 cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
 
+def clear():
+    """Emulate the 'clear screen' command based on the current OS"""
+    if name == 'nt':   # for Windows
+        _ = system('cls')
+    else:   # for mac and linux(here, os.name is 'posix')
+        _ = system('clear')
+
 def draw_cards(num_cards=2):
     """Draw random card from cards list and return a list"""
-    hand = []
+    extra_cards = []
     for draw in range(num_cards):
         card = random.choice(cards)
-        hand.append(card)
+        extra_cards.append(card)
+    return extra_cards
+
+def get_hand():
+    """Dealer automatically draws additional cards as long as sum of cards <17."""
+    hand = draw_cards()
     return hand
 
-def get_dealer_hand():
-    """
-    Dealer automatically draws additional cards as long as sum of cards <17.
-    """
-    dealer_hand = draw_cards()
-    dealer_total = add_cards(dealer_hand)
-    # Dealer hit
-    while dealer_total < 17:
-        dealer_hand += draw_cards(1)
-        dealer_total = add_cards(dealer_hand)
-        continue
-    print(f"Dealer total: {dealer_total}")  # Delete this line at final
-    return dealer_hand
-
-def get_player_hand():
-    """Draw two cards for player"""
-    player_hand = draw_cards()
-    return player_hand
-
-def player_hit():
-    print("You got another card!")
-
-def add_cards(issued_cards):
-    """Get sum total of cards"""
+def get_hand_total(issued_cards):
+    """Evaluate presence of 11 cards, make necessary adjustments then get sum total of cards"""
     total = 0
     for card in issued_cards:
         total += card
+    if 11 in issued_cards and total > 21:
+        return total-10
+
     return total
 
-
-# Dealer round 1: Show only one card.
-dealer = get_dealer_hand()
-print(dealer)    # Delete this line at final.
-print(f"Dealer hand: [{dealer[0]}, X]")
-print()
-
-# Player round1: Show first two cards and total.
-player = get_player_hand()
-player_total = add_cards(player)
-print(f"Player hand: {player}")
-print(f"Player Total: {player_total}")
-
-# Ask player to hit
-hit = input("Type 'h' to Hit, or 's' to Stay: ").lower()
-if hit == "h":
-    player_hit()
-else:
+def check_blackjack():
     pass
+
+def player_bust():
+    clear()
+    print(f"Dealer: {dealer}, Total: {dealer_total}")
+    print()
+    print(f"Player: {player}, Total: {player_total}")
+    print()
+    print("Player BUST! Dealer wins!")
+    play_again()
+
+def dealer_bust():
+    clear()
+    print(f"Dealer: {dealer}, Total: {dealer_total}")
+    print()
+    print(f"Player: {player}, Total: {player_total}")
+    print()
+    print("Dealer BUST! Player wins!")
+    play_again()
+
+def evaluate_winner():
+    clear()
+    print(f"Dealer: {dealer}, Total: {dealer_total}")
+    print()
+    print(f"Player: {player}, Total: {player_total}")
+    if dealer_total > 21:
+        dealer_bust()
+    elif dealer_total > player_total:
+        print()
+        print("Dealer wins!")
+        play_again()
+    elif dealer_total == player_total:
+        print()
+        print("It's a draw!")
+        play_again()
+    else:
+        print()
+        print("Player wins!")
+        play_again()
+
+
+def play_again():
+    again = input("Do you want to play again? (y/n): ")
+    if again == "n":
+        exit(0)
+
+
+while True:
+    clear()
+    # Player and Dealer round 1: Dealer Shows only one card, Player shows full hand.
+    dealer = get_hand()
+    dealer_total = get_hand_total(dealer)
+    if dealer_total == 21:
+        clear()
+        print(dealer)
+        print("Dealer has Blackjack. Dealer Wins!")
+        play_again()
+    else:
+        dealer_hand1 = f"Dealer hand: [{dealer[0]}, X]"
+        print(dealer_hand1)
+        print()
+
+    player = get_hand()
+    player_total = get_hand_total(player)
+    if player_total == 21 and dealer_total == 21:
+        clear()
+        print(f"Dealer hand: {dealer}, Total: {dealer_total}")
+        print()
+        print(f" Player hand: {player}, Total: {player_total}")
+        print()
+        print("Both Dealer and Player got Blackjack. Dealer WINS!")
+        play_again()
+    elif player_total == 21 and dealer_total != 21:
+        clear()
+        print(dealer)
+        print()
+        print(player)
+        print("Player has Blackjack. Player WINS!")
+        play_again()
+    else:
+        print(f"Player hand: {player}, Total: {player_total}")
+
+    # Player and dealer round 2: Player hit or stay. Dealer keeps hitting while total <17
+    keep_hitting = True
+    while keep_hitting:
+        hit = input("Player: Type 'h' to Hit, or 's' to Stay: ").lower()
+        clear()
+        if hit == "s":
+            if dealer_total > player_total:
+                evaluate_winner()
+            else:
+                keep_hitting = False
+        else:
+            player += draw_cards(1)
+            player_total = get_hand_total(player)
+            if player_total > 21:
+                player_bust()
+            else:
+                print(dealer_hand1)
+                print()
+                print(f"Player hand: {player}, Total: {player_total}")
+
+    # Evaluate dealer hand
+    dealer_total = get_hand_total(dealer)
+    while dealer_total < 17:
+        dealer += draw_cards(1)
+        print(dealer)
+        dealer_total = get_hand_total(dealer)
+
+    # Evaluate winner
+    clear()
+    evaluate_winner()
+
+
