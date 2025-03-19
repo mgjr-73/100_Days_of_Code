a5d0faf0244d49581c6947ff41d6b63891b522ac
mgjr-73 <mgjr73@outlook.com>
2025-03-19 00:21:00 -0400

    Review Day_017, lesson 121: create Question objects list

--- Days-11-20/Day_017/quiz-game-start/main.py
+++ Days-11-20/Day_017/quiz-game-start/main.py
@@ -1,15 +1,12 @@
 from question_model import Question
 from data import question_data
-from quiz_brain import QuizBrain
 
 
 question_bank = []
 
 for q in question_data:
-    question_text = q["text"]
-    question_answer = q["answer"]
-    new_question = Question(question_text, question_answer)
+    question = q["text"]
+    answer = q["answer"]
+    new_question = Question(text=question, answer=answer)
     question_bank.append(new_question)
 
-quiz = QuizBrain(question_bank)
-quiz.next_question()
