09061f92f4c3f37fa5b78bf5ec5843e27f991249
mgjr-73 <mgjr73@outlook.com>
2025-03-20 00:58:22 -0400

    Complete Day_017, lessons 122 and 123: Loop through Q's.

--- Days-11-20/Day_017/quiz-game-start/main.py
+++ Days-11-20/Day_017/quiz-game-start/main.py
@@ -1,5 +1,6 @@
 from question_model import Question
 from data import question_data
+from quiz_brain import QuizBrain
 
 
 question_bank = []
@@ -10,3 +11,8 @@ for q in question_data:
     new_question = Question(text=question, answer=answer)
     question_bank.append(new_question)
 
+
+q = QuizBrain(question_bank)
+
+while q.still_has_questions():
+    q.next_question()
--- Days-11-20/Day_017/quiz-game-start/quiz_brain.py
+++ Days-11-20/Day_017/quiz-game-start/quiz_brain.py
@@ -2,8 +2,23 @@ class QuizBrain:
     def __init__(self, qlist):
         self.question_number = 0
         self.question_list = qlist
+        self.list_length = (len(self.question_list))
+
+    def still_has_questions(self):
+        if self.list_length > 0:
+            return True
+        return False
+
+        # instructor solution:
+        # return self.question_number < len(self.question_list)
 
     def next_question(self):
-        current_question = self.question_list[self.question_number]
+        question = self.question_list[self.question_number]
         self.question_number += 1
-        input(f"Q.{self.question_number}: {current_question.text} (True/False): ")
+        self.list_length -= 1
+        input(f"Q{self.question_number}: {question.text} True or False? ")
+
+
+
+
+
