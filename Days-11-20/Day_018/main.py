865e3ead47ce3ae6c815e72892effec3278c933b
mgjr-73 <mgjr73@outlook.com>
2025-04-02 00:50:49 -0400

    Complete Day 18, Items 131 and 132

--- Days-11-20/Day_018/main.py
+++ Days-11-20/Day_018/main.py
@@ -1,28 +1,70 @@
 from turtle import Turtle, Screen
 
 
-# timmy_the_turtle = Turtle()
-# timmy_the_turtle.shape("turtle")
-# timmy_the_turtle.color("red")
+tim = Turtle()
+tim.shape("turtle")
+tim.color("red")
 
 # Challenge 1 - Draw a Square
-# timmy_the_turtle.forward(100)
-# timmy_the_turtle.right(90)
-# timmy_the_turtle.forward(100)
-# timmy_the_turtle.right(90)
-# timmy_the_turtle.forward(100)
-# timmy_the_turtle.right(90)
-# timmy_the_turtle.forward(100)
-# timmy_the_turtle.right(90)
+# tim.forward(100)
+# tim.right(90)
+# tim.forward(100)
+# tim.right(90)
+# tim.forward(100)
+# tim.right(90)
+# tim.forward(100)
+# tim.right(90)
 
 # Challenge 1 Solution
 # for _ in range(4):
-#     timmy_the_turtle.forward(100)
-#     timmy_the_turtle.left(90)
+#     tim.forward(100)
+#     tim.left(90)
 
 
-import heroes
-print(heroes.gen())
+# import heroes
+# print(heroes.gen())
+
+# Challenge 2 - Draw Dashed Line
+# for _ in range(10):
+#     tim.forward(10)
+#     tim.pu()
+#     tim.forward(10)
+#     tim.pd()
+
+# Challenge 2 Solution
+# for _ in range(15):
+#     tim.forward(10)
+#     tim.penup()
+#     tim.forward(10)
+#     tim.pendown()
+
+# Challenge 3 - Draw Shapes
+# full = 360
+# tim.forward(50)
+# for x in range(3, 37):
+#     if (full % x) == 0:
+#         angle = int(full/x)
+#         for _ in range (x):
+#             tim.right(angle)
+#             tim.forward(50)
+
+# Challenge 3 Solution
+
+# import random
+#
+# colours = ["CornflowerBlue", "DarkOrchid", "IndianRed", "DeepSkyBlue", "LightSeaGreen", "wheat", "SlateGray", "SeaGreen"]
+#
+# def draw_shape(num_sides):
+#     angle = 360 / num_sides
+#     for _ in range(num_sides):
+#         tim.forward(100)
+#         tim.right(angle)
+#
+# for shape_side_n in range(3, 10):
+#     tim.color(random.choice(colours))
+#     draw_shape(shape_side_n)
+
+
 
 
 # Screen class shows the window, otherwise you will only see it in a brief flash.
